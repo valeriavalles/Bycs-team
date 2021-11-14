faa64d2d7d299a094504f9ff6e397f4d3268b04c
Carlagreta <86120008+Carlagreta23@users.noreply.github.com>
2021-11-14 18:08:44 -0500

    ServicioSoap

---
+++ ByCsServices/ByCsServices/Entidad/Usuario1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByCsServices.Entidad
+{
+    public class Usuario1
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string correo { get; set; }
+        public string direccion { get; set; }
+
+        public string tipousuario { get; set; }
+        public string dni { get; set; }
+        public string numeroceltel { get; set; }
+        public string tiposervicio { get; set; }
+    }
+}
---
+++ ByCsServices/ByCsServices/IRegistrarUsuarioServices.cs
@@ -0,0 +1,23 @@
+﻿using ByCsServices.Entidad;
+using ByCsServices.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ByCsServices
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IRegistrarUsuarioServices" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IRegistrarUsuarioServices
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        Usuario agregarUsuario(Usuario usuario);
+    }
+
+}
